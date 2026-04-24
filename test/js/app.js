ba3e65921c1a3759c9cc2251952f59b356407af7
Metalero4-tm <tmd.web.dan@gmail.com>
2026-04-23 22:17:50 -0300

    validadcion del campos del formulario

--- test/js/app.js
+++ test/js/app.js
@@ -1,45 +1,59 @@
+// Declaración de las variables
 const inputNom = document.getElementById("nombre");
 const inputApe = document.getElementById("apellido");
 const inputEmail = document.getElementById("email");
 const inputTel = document.getElementById("telefono");
-const SelCategoria = document.getElementById("categoria");
+const selCategoria = document.getElementById("categoria");
 const inputSi = document.getElementById("cliente-si");
-const imputNo = document.getElementById("cliente-no");
+const inputNo = document.getElementById("cliente-no");
 const tAreaComent = document.getElementById("comentario");
 const btnEnviar = document.getElementById("enviar");
 const btnReset = document.getElementById("reset");
 const errorMens = document.getElementById("mensaje-error");
 const formContacto = document.querySelector("#form-contacto");
-const removeDate = () => {
+//Una expresión regular (regex) es una forma de describir patrones de texto.
+const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+/* metodo que limpia todos los campos de entradas**/
+const limpiarDatos = () => {
   inputNom.value = "";
   inputApe.value = "";
   inputEmail.value = "";
   inputTel.value = "";
-  SelCategoria.value = "";
+  selCategoria.value = "";
   inputSi.checked = false;
-  imputNo.checked = false;
+  inputNo.checked = false;
   tAreaComent.value = "";
   errorMens.textContent = "";
 };
-//Borrado de los datos del formulario
+//Se presiona el botón borra  de los datos de input del formulario
 btnReset.addEventListener("click", () => {
-  removeDate();
+  limpiarDatos();
 });
-
+// Evento que hace click , previamente valida que ningun campo este vacio
 formContacto.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  //Validacion de los campos vacios
+
   if (
     inputNom.value.trim() === "" ||
     inputApe.value.trim() === "" ||
     inputEmail.value.trim() === "" ||
     inputTel.value.trim() === "" ||
-    SelCategoria.value === "" ||
+    selCategoria.value === "" ||
     tAreaComent.value.trim() === "" ||
-    (!inputSi.checked && !imputNo.checked)
+    (!inputSi.checked && !inputNo.checked)
   ) {
     errorMens.textContent = "Campos Vacios";
+
+    return false;
+  }
+  //validacion del formato
+  if (!regex.test(inputEmail.value)) {
+    errorMens.textContent = "El formato no es valido, debe usar @";
+    return false;
   } else {
     alert("El formulario fue enviado");
-    removeDate();
+    limpiarDatos();
   }
 });
